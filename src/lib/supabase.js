429d79bcc6e705f2626de04d0563b95f921bf61b
Dhanvish <sjdhanvish@gmail.com>
2026-07-06 08:25:04 +0530

    added annon

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl = "https://rlpeuraolsjfawvcchki.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJscGV1cmFvbHNqZmF3dmNjaGtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MzM5NjIsImV4cCI6MjA2ODA5OTk2Mn0.V_58f67Oa_y239pT37O-m674Fof0XlR3iR1h81Dq-Xk";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
